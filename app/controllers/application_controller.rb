67d454683ae81e23bd103834a6b24c808cebe683
seneiya <seneiyacooke@gmail.com>
2021-11-02 13:56:13 -0400

    commit tree 40d6d42e15f7963ae4a61bbf70cdf147abfe502c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,24 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  def index
+    plants = Plant.all
+    render json: plants
+  end
+
+  def show
+    plant = Plant.find_by(id: params[:id])
+    render json: plant
+  end
+
+  def create
+    plant = Plant.create(plant_params)
+    render json: plant, status: :created
+  end
+
+  private
+
+  def plant_params
+    params.permit(:name, :image, :price)
+  end
 end
